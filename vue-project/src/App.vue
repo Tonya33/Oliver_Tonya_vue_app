f3e6453f56114d06339f4c0a77a4cd5f43e17018
Tonya Oliver <90284705+Tonya33@users.noreply.github.com>
2023-05-18 16:56:24 -0400

    getting project to a starting point for development

---
+++ vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/birdie-logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Hello and Welcome to tonya's app!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
